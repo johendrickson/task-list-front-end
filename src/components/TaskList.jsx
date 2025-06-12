48f3157784cdc51a8251ba64386928dd59320d85
Jamie Hendrickson <jamie.o.hendrickson@gmail.com>
2025-06-12 15:00:56 -0700

    Refactors TaskList to lift state up

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import Task from './Task.jsx';
 import './TaskList.css';
 
-const TaskList = ({ tasks }) => {
+const TaskList = ({ tasks, onToggleTask, onDeleteTask }) => {
   const getTaskListJSX = (tasks) => {
     return tasks.map((task) => {
       return (
@@ -11,11 +11,18 @@ const TaskList = ({ tasks }) => {
           id={task.id}
           title={task.title}
           isComplete={task.isComplete}
+          onToggleTask={onToggleTask}
+          onDeleteTask={onDeleteTask}
         />
       );
     });
   };
-  return <ul className="tasks__list no-bullet">{getTaskListJSX(tasks)}</ul>;
+
+  return (
+    <ul className="tasks__list no-bullet">
+      {getTaskListJSX(tasks)}
+    </ul>
+  );
 };
 
 TaskList.propTypes = {
@@ -26,6 +33,8 @@ TaskList.propTypes = {
       isComplete: PropTypes.bool.isRequired,
     })
   ).isRequired,
+  onToggleTask: PropTypes.func.isRequired,
+  onDeleteTask: PropTypes.func.isRequired,
 };
 
 export default TaskList;
