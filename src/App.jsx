1a3a89a9f66d58aa195f846d6cf80499c78d006b
Jamie Hendrickson <jamie.o.hendrickson@gmail.com>
2025-06-12 23:10:55 -0700

    lifts state to App; implements toggle and delete callback functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-// import { useState } from 'react';
 import TaskList from './components/TaskList.jsx';
 import './App.css';
+import { useState } from 'react';
 
-const TASKS = [
+const initialTasks = [
   {
     id: 1,
     title: 'Mow the lawn',
@@ -15,14 +15,50 @@ const TASKS = [
   },
 ];
 
-const App = () => {
+function App() {
+  const [tasks, setTasks] = useState(initialTasks);
+
+  const toggleTaskComplete = (id) => {
+    // copy state of `tasks` into a new variable which can be changed and set as new/updated state of `tasks`
+    const newStateOfTasks = [...tasks];
+
+    // array.find( anonymous arrow function which returns `true` or `false` for each item in the array )
+    const indexOfTaskToAlter = newStateOfTasks.findIndex((task) => id === task.id);
+
+    // toggle the boolean state of the relevant task's `isComplete` property
+    newStateOfTasks[indexOfTaskToAlter].isComplete = !newStateOfTasks[indexOfTaskToAlter].isComplete;
+
+    // actually change `tasks`
+    setTasks(newStateOfTasks);
+  };
+
+  const deleteTask = (id) => {
+    // copy state of `tasks` into a new variable which can be changed and set as new/updated state of `tasks`
+    const newStateOfTasks = [...tasks];
+
+    // array.find( anonymous arrow function which returns `true` or `false` for each item in the array )
+    const indexOfTaskToAlter = newStateOfTasks.findIndex((task) => id === task.id);
+
+    // remove the indicated task from new/updated state of `tasks`
+    newStateOfTasks.splice(indexOfTaskToAlter, 1);
+
+    // actually change `tasks`
+    setTasks(newStateOfTasks);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <TaskList
+          tasks={tasks}
+          onToggleTask={toggleTaskComplete}
+          onDeleteTask={deleteTask}
+        />
+
       </main>
     </div>
   );
