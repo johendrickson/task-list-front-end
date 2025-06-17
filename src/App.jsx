b1b99720d59a6063f5130d0c98180237fdddffff
Jamie Hendrickson <jamie.o.hendrickson@gmail.com>
2025-06-17 11:33:25 -0700

    Adds in useEffect to App and axios GET request

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,41 @@ import TaskList from './components/TaskList.jsx';
 import './App.css';
 import { useState, useEffect } from 'react';
 
-const initialTasks = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+const API_BASE_URL = 'http://localhost:5000/tasks';
+
+// const initialTasks = [
+//   {
+//     id: 1,
+//     title: 'Mow the lawn',
+//     isComplete: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Cook Pasta',
+//     isComplete: true,
+//   },
+// ];
+
+const taskApiToJson = (task) => {
+  const {description, id, is_complete: isComplete, title} = task;
+  return {description, id, isComplete, title};
+};
 
 function App() {
-  const [tasks, setTasks] = useState(initialTasks);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${API_BASE_URL}/tasks`)
+      .then((response) => {
+        const tasksFromApi = response.data.map(taskApiToJson);
+        setTasks(tasksFromApi);
+      })
+      .catch((err) => {
+        console.error('Error fetching tasks:', err);
+      });
+  }, []);
+
+
 
   const toggleTaskComplete = (id) => {
     // copy state of `tasks` into a new variable which can be changed and set as new/updated state of `tasks`
